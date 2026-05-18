0549ac3949964a7f012d96ce9fea1a6b5eb8759a
Pooja bhavani <147735975+pooja-bhavani@users.noreply.github.com>
2026-05-19 03:05:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+const express = require("express");
+
+const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Dev First Mate Running 🚀");
+});
 
 app.listen(PORT, () => {
   console.log(`Dev First Mate running on ${PORT}`);
